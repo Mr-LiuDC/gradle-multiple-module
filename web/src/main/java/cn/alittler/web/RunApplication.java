07afd99020d076d432bd2dea3a475ad64dbe8ecc
liudecai <liudecai@postop.cn>
2018-03-29 18:19:16 +0800

    project initial

---
+++ web/src/main/java/cn/alittler/web/RunApplication.java
@@ -0,0 +1,19 @@
+package cn.alittler.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RunApplication
+ *
+ * @author LiuDeCai
+ * @date 2018/03/29
+ */
+@SpringBootApplication
+public class RunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/cn/alittler/web/RunApplicationTest.java
@@ -0,0 +1,23 @@
+package cn.alittler.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * RunApplicationTest
+ *
+ * @author LiuDeCai
+ * @date 2018/03/29
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunApplicationTest {
+
+    @Test
+    public void contentLoad() {
+
+    }
+
+}
