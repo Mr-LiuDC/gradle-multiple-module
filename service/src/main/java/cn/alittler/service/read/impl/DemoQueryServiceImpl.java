e2d4b1aca70f074f3ffbd8de84c7a0eccd657776
liudecai <liudecai@postop.cn>
2018-04-10 20:54:46 +0800

    [Change]更新测试

--- service/src/main/java/cn/alittler/service/read/impl/DemoQueryServiceImpl.java
+++ service/src/main/java/cn/alittler/service/read/impl/DemoQueryServiceImpl.java
@@ -4,9 +4,13 @@ import cn.alittler.dto.DemoDto;
 import cn.alittler.entity.DemoEntity;
 import cn.alittler.repository.DemoRepository;
 import cn.alittler.service.read.DemoQueryService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * DemoQueryServiceImpl
@@ -21,11 +25,17 @@ public class DemoQueryServiceImpl implements DemoQueryService {
 
     @Override
     public DemoDto getDemoById(Long id) {
-        return (DemoDto) demoRepository.findOne(id);
+        DemoEntity demoEntity = demoRepository.getOne(id);
+        DemoDto demoDto = new DemoDto();
+        BeanUtils.copyProperties(demoEntity, demoDto);
+        return demoDto;
     }
 
     @Override
     public Page<DemoEntity> getDemoList(Pageable pageable) {
-        return demoRepository.findAll(pageable);
+        Page<DemoEntity> demoEntityPage = demoRepository.findAll(pageable);
+        List<DemoEntity> demoEntityList = demoEntityPage.getContent();
+        PageImpl<DemoEntity> demoDtoPage = new PageImpl<>(demoEntityList);
+        return demoDtoPage;
     }
 }
